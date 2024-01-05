faeec45a5191b7a1420565aa330a357deaee0f5a
JoyBoyCr7 <Rnyongkah@gmail.com>
2024-01-05 01:06:20 -0600

    mongoose

--- iphonebe.js
+++ iphonebe.js
@@ -1,13 +1,22 @@
 const express =  require("express")
-
+const mongoose = require("mongoose")
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT
 
-const aa = "d"
+const {PORT, DATABASEURL} = process.env
+
+mongoose.connect(DATABASEURL)
+mongoose.connection
+    .on("open" ,() => console.log('Connected to db'))
+    .on("Close" ,() => console.log('Disonnected from db'))
+    .on("Error" ,(Error) => console.log('Error connecting to db'))
 
 app.get('/', (req, res) => {
     res.send("Ronard off to good start")
 })
 
-app.listen(port, () => {console.log(`listening on port ${port}`)})
+
+
+
+
+app.listen(PORT, () => {console.log(`listening on port ${PORT}`)})
